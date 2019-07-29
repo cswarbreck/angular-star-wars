f68eebf127a3e12f540ad0f0cf573f236475a6d1
Cosmo Swarbreck <c.swarbreck@gmail.com>
2019-07-29 14:05:26 +0100

    Added list, item and tabs components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ListComponent } from './list/list.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ListComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule
