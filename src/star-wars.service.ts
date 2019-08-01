94ec2a8cdb732a800021f4c4f7bf1d99b77a2ea8
Cosmo Swarbreck <c.swarbreck@gmail.com>
2019-08-01 17:10:11 +0100

    Adds in validation to star wars service to check if a character is already there.

--- src/star-wars.service.ts
+++ src/star-wars.service.ts
@@ -28,4 +28,17 @@ export class StarWarsService {
     this.characters[pos].side = charInfo.side;
     this.logService.writeLog('Changed side of ' + charInfo.name + ', new side: ' + charInfo.side);
   }
+  addCharacter(name, side) {
+    const pos = this.characters.findIndex((char) => {
+      return char.name === name;
+    });
+    if (pos !== -1) {
+      return;
+    }
+    const newChar = {
+      name: name,
+      side: side
+    };
+    this.characters.push(newChar);
+  }
 }
